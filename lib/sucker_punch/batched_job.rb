2f9d88526f66ab4fba62597605c49a5cc0a9293e
Richard Bishop <rbishop87@gmail.com>
2014-10-20 13:19:31 -0600

    Move methods down into ClassMethods

--- lib/sucker_punch/batched_job.rb
+++ lib/sucker_punch/batched_job.rb
@@ -11,24 +11,16 @@ module SuckerPunch
           define_celluloid_pool(self, @workers)
           SuckerPunch::BatchedQueue.new(self)
         end
-
-        def self.xyz_after_batch(klass)
-          @after_batch = klass
-        end
-
-        def self.oogabooga
-          puts "oogabooga"
-        end
-
-        def self.after_batch
-          @after_batch
-        end
       end
     end
 
     module ClassMethods
       def workers(num)
         @workers = num
+      end
+
+      def self.after_batch
+        @after_batch
       end
 
       def define_celluloid_pool(klass, num_workers)
